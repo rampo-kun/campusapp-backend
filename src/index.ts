ae4844e20d6d17578526692909c36aab8bbc04df
rampo-kun <106396261+rampo-kun@users.noreply.github.com>
2024-09-22 17:30:18 +0530

    implementing get

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 4000;
 
+app.get("/hello", (request, response) => {
+  response.json({ message: "hello", status: "200" });
+});
+
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}`);
 });
